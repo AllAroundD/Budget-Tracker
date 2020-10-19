bbb99b7dd250bc27f974edaee93ce6af159f4a96
AllAroundD <AllAroundDWeb@gmail.com>
2020-10-19 17:21:10 -0400

    updated config var for mongo migration

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
-require('dotenv').config()      // loads confirmation information from the .env file
-const express = require("express")
-const logger = require("morgan")
-const mongoose = require("mongoose")
-const compression = require("compression")
+require('dotenv').config() // loads confirmation information from the .env file
+const express = require('express')
+const logger = require('morgan')
+const mongoose = require('mongoose')
+const compression = require('compression')
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
 
-app.use(logger("dev"))
+app.use(logger('dev'))
 
 app.use(compression())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.use(express.static("public"))
+app.use(express.static('public'))
 
-mongoose.connect(process.env.MONGODB_URI || process.env.DB_URL,
+mongoose.connect(process.env.DB_URI || process.env.DB_URL,
   { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }, (err) => {
-    if (err) throw err
-    console.log("DB Connected Successfully")
+    if (err) {
+      throw err
+    }
+    console.log('DB Connected Successfully')
   })
 
 // routes
-app.use(require("./routes/api.js"))
+app.use(require('./routes/api.js'))
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`)
